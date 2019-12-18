7b5d0c1c96f00d9e7ea2198c059e98c574522a0e
lvhquan <lvhquan0110@gmail.com>
2019-12-18 19:36:19 +0700

    update logo website

--- src/containers/DefaultLayout/DefaultHeader.js
+++ src/containers/DefaultLayout/DefaultHeader.js
@@ -6,7 +6,7 @@ import {  UncontrolledDropdown, DropdownItem, DropdownMenu, DropdownToggle, Nav,
 // import PropTypes from 'prop-types';
 
 import { AppNavbarBrand, AppSidebarToggler } from '@coreui/react';
-import logo from '../../assets/img/brand/logo.svg'
+import logo from '../../assets/img/brand/Icons8_flat_multiple_smartphones.svg'
 import sygnet from '../../assets/img/brand/sygnet.svg'
 import {appConfig} from "../../configs/app.config"
 
@@ -32,8 +32,9 @@ class DefaultHeader extends Component {
       <React.Fragment>
         <AppSidebarToggler className="d-lg-none" display="md" mobile />
         <AppNavbarBrand
-          full={{ src: logo, width: 89, height: 25, alt: 'CoreUI Logo' }}
-          minimized={{ src: sygnet, width: 30, height: 30, alt: 'CoreUI Logo' }}
+          full={{ src: logo, width: 89, height: 50, alt: 'CoreUI Logo' }}
+          // full={{ src: logo, width: 89, height: 25, alt: 'CoreUI Logo' }}
+          // minimized={{ src: sygnet, width: 30, height: 30, alt: 'CoreUI Logo' }}
         />
         <AppSidebarToggler className="d-md-down-none" display="lg" />
 
